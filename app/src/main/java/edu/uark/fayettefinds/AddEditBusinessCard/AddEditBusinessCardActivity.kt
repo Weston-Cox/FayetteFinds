dd411fcbe6a9a2a4e89d200101908dabee58c6e4
Weston Cox <wescox04@icloud.com>
2024-12-01 22:25:54 -0600

    Added Layout files for the Activities' views.

--- app/src/main/java/edu/uark/fayettefinds/AddEditBusinessCard/AddEditBusinessCardActivity.kt
+++ app/src/main/java/edu/uark/fayettefinds/AddEditBusinessCard/AddEditBusinessCardActivity.kt
@@ -2,5 +2,5 @@ package edu.uark.fayettefinds.AddEditBusinessCard
 
 import androidx.appcompat.app.AppCompatActivity
 
-class AddEditBusinessCardActivity{
+class AddEditBusinessCardActivity: AppCompatActivity() {
 }
